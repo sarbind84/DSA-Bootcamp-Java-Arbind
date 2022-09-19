c996c4a167e44320564ec3a384e1617d3ec9fe61
arbindadvatix <arbind.sharma@advatix.com>
2022-09-19 23:05:17 +0530

    hello c

--- src/main/java/com/example/source_dsa/Patterns/hallowButterfly.java
+++ src/main/java/com/example/source_dsa/Patterns/hallowButterfly.java
@@ -18,15 +18,58 @@ public class hallowButterfly {
                 }
             }
 
-
             int spaces = 2 * (n-i)-1;
             for(int j=1; j<spaces; j++) {
                 System.out.print("-");
             }
 
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || i == j) {
+                    System.out.print("*");
+                } else {
+                    System.out.print("-");
+                }
+            }
             System.out.println();
-
         }
+
+        //lower part
+
+
+        for (int i = n-1; i >= 1; i--) {
+
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || i == j) {
+                    System.out.print("*");
+                } else {
+                    System.out.print("-");
+                }
+            }
+
+           int spaces = 2 * (n-i)-1;
+            for(int j=1; j<spaces; j++) {
+                System.out.print("-");
+            }
+
+
+            for(int j=1; j<=i; j++) {
+                if (j == 1 || i == j) {
+                    System.out.print("*");
+                } else {
+                    System.out.print("-");
+                }
+            }
+
+            System.out.println();
+        }
+
+
+
+
+
+
+
+
 
     }
 }
