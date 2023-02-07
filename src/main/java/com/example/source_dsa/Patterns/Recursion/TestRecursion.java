8f2a268bdcb33fd2791d641ddec1a521ecaddd18
arbindadvatix <arbind.sharma@advatix.com>
2023-02-07 20:50:39 +0530

    recursion

---
+++ src/main/java/com/example/source_dsa/Patterns/Recursion/TestRecursion.java
@@ -0,0 +1,17 @@
+package com.example.source_dsa.Patterns.Recursion;
+
+public class TestRecursion {
+    public static void main(String[] args) {
+        //lifo.. stack
+        fun(5);
+    }
+
+    private static void fun(int n) {
+        if(n==0){
+            return;
+        }
+        System.out.println("Before Stack "+ "f(" +n +")");
+        fun(n-1);
+        System.out.println("After Stack calls removed out of stack"+n);
+    }
+}
---
+++ src/main/java/com/example/source_dsa/Patterns/Recursion/reverseANum.java
@@ -0,0 +1,30 @@
+package com.example.source_dsa.Patterns.Recursion;
+
+public class reverseANum {
+    public static void main(String[] args) {
+
+        int num = 341;
+
+
+        System.out.println(rev(431));
+
+
+    }
+
+    public static int rev(int x) {
+        int digit = (int) (Math.log10(x) + 1);
+
+        return helper(x, digit);
+    }
+
+    private static int helper(int n, int digit) {
+
+        if (n % 10 == n) {
+            return n;
+        }
+        int rem = n % 10;
+        return rem * (int) Math.pow(10, digit - 1) + helper(n / 10, digit - 1);
+    }
+
+
+}
