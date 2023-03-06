ecd278dc565349e7fe35d9231195610b8198d5f6
arbindadvatix <arbind.sharma@advatix.com>
2023-03-06 12:28:45 +0530

    notes updated1

---
+++ src/main/java/com/example/source_dsa/Patterns/mainLL.java
@@ -0,0 +1,19 @@
+package com.example.source_dsa.Patterns;
+
+/**
+ * @Author : ArbindSharma
+ * @Date : 01-03-2023
+ **/
+public class mainLL {
+
+    public static void main(String[] args) {
+        LL l = new LL();
+        l.insertFirst(4);
+        l.insertFirst(5);
+        l.insertFirst(6);
+        l.insertLast(2);
+
+        l.display();
+    }
+
+}
